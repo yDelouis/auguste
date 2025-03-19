d3d0970130e14fb41b8ba06316e2ef2be725bf80
Yoann Delouis <ydelouis@gmail.com>
2025-03-19 09:06:25 +0100

    ajout de la logique du jeu

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -16,12 +16,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,25 +34,56 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapp.ui.theme.MyAppTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
+    var ecranModel by mutableStateOf(
+        EcranModel(
+            bulles = listOf(Bulle(text = "Devine le nombre", Position.Gauche)),
+            nombre = null
+        )
+    )
+
+    var randomNumber = Random.nextInt(0, 10001) // La borne supérieure (10001) est exclusive
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            App(ecranModel, ::onNumberChange, ::onvalidate)
         }
+    }
+
+    fun onNumberChange(number: Int?) {
+        var newEcranModel = ecranModel.copy(nombre = number)
+        ecranModel = newEcranModel
+    }
+
+    fun onvalidate() {
+        var nombre = ecranModel.nombre ?: return
+        var reponse = if (nombre > randomNumber) "C'est moins"
+                    else if (nombre < randomNumber) "C'est plus"
+                    else "Bien joué!"
+        var newEcranModel = ecranModel.copy(
+            bulles = ecranModel.bulles
+                    + Bulle(text = ecranModel.nombre.toString(), Position.Droite)
+                    + Bulle(text = reponse, Position.Gauche),
+            nombre = null
+        )
+        ecranModel = newEcranModel
+
     }
 }
 
 data class EcranModel(
     val bulles: List<Bulle>,
-    val nombre: Int
+    val nombre: Int?
 )
 
 data class Bulle(
     val text: String,
-    val position : Position
+    val position: Position
 )
 
 enum class Position {
@@ -57,22 +92,43 @@ enum class Position {
 
 
 @Composable
-fun App(ecranModel: EcranModel, modifier: Modifier = Modifier) {
+fun App(
+    ecranModel: EcranModel,
+    onNumberChange: (Int?) -> Unit,
+    onvalidate: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     MyAppTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Ecran(ecranModel, modifier.padding(innerPadding))
+            Ecran(
+                ecranModel,
+                onNumberChange,
+                onvalidate,
+                modifier.padding(innerPadding)
+            )
         }
     }
 }
 
 @Composable
-fun Ecran(ecranModel: EcranModel, modifier: Modifier = Modifier) {
+fun Ecran(
+    ecranModel: EcranModel,
+    onNumberChange: (Int?) -> Unit,
+    onvalidate: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(modifier) {
-        LazyColumn(modifier = Modifier.fillMaxWidth().weight(1f), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            items(ecranModel.bulles) { bulle: Bulle ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            reverseLayout = true,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(ecranModel.bulles.reversed()) { bulle: Bulle ->
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = if(bulle.position == Position.Gauche) Arrangement.Start else Arrangement.End
+                    horizontalArrangement = if (bulle.position == Position.Gauche) Arrangement.Start else Arrangement.End
                 ) {
                     Text(
                         bulle.text,
@@ -80,7 +136,8 @@ fun Ecran(ecranModel: EcranModel, modifier: Modifier = Modifier) {
                             .background(
                                 color = Color.Gray,
                                 shape = RoundedCornerShape(8.dp)
-                            ).padding(horizontal = 16.dp, vertical = 8.dp)
+                            )
+                            .padding(horizontal = 16.dp, vertical = 8.dp)
                     )
                 }
 
@@ -88,19 +145,23 @@ fun Ecran(ecranModel: EcranModel, modifier: Modifier = Modifier) {
         }
         Row(modifier = Modifier.padding(16.dp)) {
             TextField(
-                value = ecranModel.nombre.toString(),
+                value = if (ecranModel.nombre == null) "" else ecranModel.nombre.toString(),
                 onValueChange = { newText ->
-
+                    var newnumber = newText.toIntOrNull()
+                    if (newnumber!=null) onNumberChange(newnumber)
+                    else if (newText.isBlank()) onNumberChange(null)
                 },
                 label = { Text("Entrez un nombre") },
                 keyboardOptions = KeyboardOptions.Default.copy(
                     keyboardType = KeyboardType.Number
                 ),
+                singleLine = true,
+                keyboardActions = KeyboardActions(onDone = { onvalidate() }),
                 modifier = Modifier.weight(1f)
             )
 
             Button(
-                onClick = { /* Action pour valider le nombre ou faire quelque chose avec le texte */ },
+                onClick = { onvalidate() },
                 modifier = Modifier.padding(top = 16.dp)
             ) {
                 Text("Valider")
@@ -120,6 +181,6 @@ fun AppPreview() {
         ),
         nombre = 145
     )
-    App(ecranModel)
+    App(ecranModel, {}, {})
 
 }
